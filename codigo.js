588a2e9b2d19eed251e84c4bf1f6844ab7268c1c
eduardocastro31 <112891575+eduardocastro31@users.noreply.github.com>
2023-06-23 17:58:35 -0300

    cambios js

--- codigo.js
+++ codigo.js
@@ -3,69 +3,84 @@ var form = document.querySelector("form")
 var footer = document.querySelector("footer")
 var submit = document.getElementById("boton-input");
 var nombre = document.getElementById("nombre")
-var boton = document.getElementById("liquidacion");
+var liqui = document.getElementById("liquidacion");
 var eMail = document.getElementById("correo");
 var telefono = document.getElementById("telefono");
 var texto = document.getElementById("texto");
+
 let dataArr = [];
+const div = document.createElement("div");
+div.id = "di"
 
 submit.addEventListener('click', function () {
+
     nombreEmpresa = nombre.value
-    sueldo = boton.value;
+    sueldo = liqui.value;
     correo = eMail.value;
     contacto = telefono.value;
     trabajo = texto.value;
 
+    const button = document.createElement("button");
+    button.innerHTML = "Ordenar";
+    button.id = "ordena";
+    button.className = "btn btn-warning";
+    var boton = document.getElementById("ordena");
 
     const ul = document.createElement("ul");
-    const uli = document.getElementById("creado");
-    ul.id = "creado";
+    const uli = document.getElementsByClassName("creado");
+    const dir = document.getElementById("di")
+    ul.className = "creado";
 
+    var valido = true;
+    ul.setAttribute('style', 'list-style:none;');
+    div.remove();
     if (contacto == "" || correo == "") {
         alert("COMPLETE TODOS LOS DATOS");
+        valido = false;
     }
 
-    if (sueldo > 100000) {
+    if (sueldo > 100000 && valido == true) {
+        footer.appendChild(div);
 
-        footer.appendChild(ul);
-
+        div.appendChild(ul);
         dataArr.push({
             nombre: nombreEmpresa,
             eMail: correo,
-            boton: sueldo,
+            liqui: sueldo,
         });
 
 
-        dataArr.sort((a, b) => a.boton - b.boton);
         dataArr.map((obj) => {
-            const li = document.createElement("li");
 
             dolar = (obj.boton / 500).toFixed(2);
-            ul.setAttribute('style', 'list-style:none;');
-
             ul.innerText = dataArr.indexOf(obj) + 1;
-
             let li1 = document.createElement('li');
-            li1.innerHTML = "Empresa: " + nombreEmpresa;
+            li1.innerHTML = "Empresa: " + obj.nombre;
             let li2 = document.createElement('li');
-            li2.innerHTML = "Correo: " + correo;
+            li2.innerHTML = "Correo: " + obj.eMail;
             let li3 = document.createElement("li");
-            li3.innerHTML = "Sueldo : " + sueldo;
+            li3.innerHTML = "Sueldo : " + obj.liqui;
+            let li4 = document.createElement("li")
+            li4.innerHTML = "Dolar : USSD " + (obj.liqui * 500);
+            let li5 = document.createElement("li");
+            li5.innerHTML = "Euro : e " + (obj.liqui * 520);
 
             ul.appendChild(li1);
             ul.appendChild(li2);
             ul.appendChild(li3);
+            ul.appendChild(li4);
+            ul.appendChild(li5);
 
+            form.reset();
         });
 
         console.log(dataArr);
 
 
-        if (document.body.contains(uli) == false) {
+        if (document.body.contains(dir) == false) {
+            form.appendChild(button);
             ordenar();
         }
-
-        form.reset();
     }
 
     else {
@@ -73,16 +88,50 @@ submit.addEventListener('click', function () {
         form.reset();
     }
 
+
+
+
     function ordenar() {
-        const button = document.createElement("button");
-        button.innerHTML = "Ordenar";
-        button.id = "ordena";
-        button.className = "btn btn-warning";
-        var boton = document.getElementById("ordena");
-        form.appendChild(button);
+
 
         button.addEventListener('click', function () {
-            
+            ascender = true;
+            button.remove();
+            div.remove();
+
+            dataArr.sort((a, b) => b.liqui - a.liqui);
+
+            if (ascender == true) {
+                footer.appendChild(div);
+
+                div.appendChild(ul);
+
+                console.log(dataArr);
+
+                dataArr.map((obj) => {
+                    dolar = (obj.liqui / 500).toFixed(2);
+                    ul.innerText = dataArr.indexOf(obj) + 1;
+                    console.log(obj.liqui);
+                    let li1 = document.createElement('li');
+                    li1.innerHTML = "Empresa: " + obj.nombre;
+                    let li2 = document.createElement('li');
+                    li2.innerHTML = "Correo: " + obj.eMail;
+                    let li3 = document.createElement("li");
+                    li3.innerHTML = "Sueldo : " + obj.liqui;
+                    li4.innerHTML = "Dolar : USSD " + (obj.liqui * 500);
+                    let li5 = document.createElement("li");
+                    li5.innerHTML = "Euro : e " + (obj.liqui * 520);
+
+                    ul.appendChild(li1);
+                    ul.appendChild(li2);
+                    ul.appendChild(li3);
+                    ul.appendChild(li4);
+                    ul.appendChild(li5);
+                    form.reset();
+
+                });
+            }
+
         });
 
     }
